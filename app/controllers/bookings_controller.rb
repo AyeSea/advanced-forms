8b4893e1b7517d54fb2fd1292ed10be9f1529681
Andrew Chung <andrew.chung159@gmail.com>
2015-04-13 15:20:58 -0400

    Add nested form for creating booking/passenger (need to refactor)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,45 @@
 class BookingsController < ApplicationController
 	def new
 		@flight = Flight.find_by(id: params[:flight_id])
-		@num_passengers = params[:passengers]
+		@booking = []
+		params[:passengers] = "1" if params[:passengers].blank?
+		params[:passengers].to_i.times { @booking << @flight.bookings.build }
 	end
+
+	def create
+		@flight = Flight.find_by(id: params[:flight_id])
+		@passenger = Passenger.new(name: params[:booking][:passengers][:name],
+															 email: params[:booking][:passengers][:email])
+
+		if @passenger.save
+			@booking = @flight.bookings.build(passenger_id: @passenger.id)
+
+			if @booking.save && @passenger.save
+				flash[:success] = 'Flight was successfully booked!'
+				redirect_to root_path
+			else
+				flash.now[:error] = 'Something went wrong! Please try again.'
+				render 'new'
+			end
+		else
+			flash.now[:error] = 'Something went wrong! Please try again.'
+			render 'new'
+		end
+	end
+
+	def index
+	end
+
+=begin
+#whitelist params after figuring out how to get them from browser first
+	private
+
+		def booking_params
+			params.require(:booking).permit(passenger_params)
+		end
+
+		def passenger_params
+			params.require(:booking).require(:passengers).permit(:name, :email)
+		end
+=end
 end
